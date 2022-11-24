6f150087d0a05db0d11a1289d33d2bf6e7f358ec
Miguel <salixasesores@gmail.com>
2022-11-24 01:00:24 +0100

    Basic nav bar added

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,55 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <v-toolbar 
+        height="120px"
+        color="#f6fcf5"
+      >
+      <v-spacer></v-spacer>
+      <v-toolbar-side-icon>
+        <img style="height:100px" :src="require('./assets/logo.png')">
+      </v-toolbar-side-icon>
+        <v-spacer></v-spacer>
+        <v-btn class="hidden-sm-and-down" variant="text" color="green">INICIO</v-btn>
+        <v-btn class="hidden-sm-and-down" variant="text" color="green">NOSOTROS</v-btn>
+        <v-btn class="hidden-sm-and-down" variant="text" color="green">SERVICIOS</v-btn>
+        <v-btn class="hidden-sm-and-down" variant="text" color="green">EQUIPO</v-btn>
+        <v-btn class="hidden-sm-and-down" variant="text" color="green">CONTACTO</v-btn>
+        <v-btn class="hidden-sm-and-down" variant="outlined" color="teal">ACCESO CLIENTES</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon @click.stop="drawer = !drawer">
+          <v-icon class="hidden-sm-and-up">mdi-menu</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-navigation-drawer
+        v-model="drawer"
+        location="right"
+        temporary
+      >
+        <v-list>
+          <v-divider></v-divider>
+          <v-list-item>
+            XD
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    drawer: false,
+    group: null,
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
   }),
 }
 </script>
