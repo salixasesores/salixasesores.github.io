2fa0a78f0a26f2810b954548acddd8b40871c3c7
Miguel <salixasesores@gmail.com>
2022-11-30 13:03:13 +0100

    Added sidebar

--- src/App.vue
+++ src/App.vue
@@ -26,13 +26,105 @@
         location="right"
         temporary
       >
-        <v-list>
-          <v-divider></v-divider>
-          <v-list-item>
-            XD
-          </v-list-item>
+        <v-list :items="barItems">
+          <v-list-item title="Inicio" value="1" prepend-icon="mdi-home"/>
+          <v-list-item title="Nosotros" value="2" prepend-icon="mdi-flower"/>
+          <v-list-item title="Servicios" value="3" prepend-icon="mdi-star"/>
+          <v-list-item title="Equipo" value="4" prepend-icon="mdi-account-multiple"/>
+          <v-list-item title="Contacto" value="5" prepend-icon="mdi-email"/>
+          <v-list-item title="Acceso Clientes" value="6" prepend-icon="mdi-account"/>
+          <v-list-item title="Cerrar" value="7" prepend-icon="mdi-keyboard-return" @click.stop="drawer = !drawer"/>
         </v-list>
       </v-navigation-drawer>
+
+      <v-carousel show-arrows="hover" height="350px" cycle="true">
+        <v-carousel-item
+          src="https://d2jx2rerrg6sh3.cloudfront.net/image-handler/ts/20220131080401/ri/750/src/images/Article_Images/ImageForArticle_22064_16436342404173431.jpg"
+          cover
+          gradient="to top right, rgba(220,220,220,.7), rgba(230,255,230,.7)"
+          transparency="0.5"
+        >
+        <div>
+            <div style="margin-left: 5%; margin-right: 5%; margin-top: 10px; font-weight: bold; font-size: 30px;">
+              Asesoría de seguridad alimentaria, control de aguas y formación
+            </div>
+            <div style="margin-left: 5%; margin-right: 5%; font-size: 20px">
+              Servicio integral con toma de muestras, análisis, emisión de informes y asesoría
+            </div>
+          </div>
+      </v-carousel-item>
+      <v-carousel-item
+          src="https://lmg-labmanager.s3.amazonaws.com/assets/articleNo/27442/aImg/50021/lab-quality-assurance-l.jpg"
+          cover
+          gradient="to top right, rgba(220,220,220,.7), rgba(230,255,230,.7)"
+          transparency="0.5"
+        >
+        <div>
+            <div style="margin-left: 5%; margin-right: 5%; margin-top: 10px; font-weight: bold; font-size: 30px;">
+              Aquí va otro texto diferente pero más guay aún
+            </div>
+            <div style="margin-left: 5%; margin-right: 5% font-size: 20px">
+              Con otro subtitulo también bien chulo
+            </div>
+          </div>
+      </v-carousel-item>
+      <v-carousel-item
+          src="https://iq-laboratory.com/wp-content/uploads/2021/04/laboratory-design.jpg"
+          cover
+          gradient="to top right, rgba(220,220,220,.7), rgba(230,255,230,.7)"
+          transparency="0.5"
+        >
+        <div>
+            <div style="margin-left: 5%; margin-right: 5%; margin-top: 10px; font-weight: bold; font-size: 30px;">
+              Aquí el último texto.
+            </div>
+            <div style="margin-left: 5%; margin-right: 5% font-size: 20px">
+              Cada vez mejor
+            </div>
+          </div>
+      </v-carousel-item>
+      </v-carousel>
+      <v-container grid-list-xs>
+        <v-row class="justify-center">
+          <div style="font-weight: bold; font-size:30px">
+            ¿Qué te ofrece Sálix?
+          </div>
+        </v-row>
+        <br>
+        <br>
+        <v-row class="justify-space-between">
+          <v-spacer></v-spacer>
+
+          <v-card width="350px" height="350px" color="green">
+            <v-icon>mdi-home</v-icon>
+            <v-card-title>
+              Titulo
+            </v-card-title>
+            <v-card-subtitle>
+              Subtitle
+            </v-card-subtitle>
+          </v-card>
+          <v-card width="350px" height="350px" color="green">
+            <v-icon>mdi-home</v-icon>
+            <v-card-title>
+              Titulo
+            </v-card-title>
+            <v-card-subtitle>
+              Subtitle
+            </v-card-subtitle>
+          </v-card>
+          <v-card width="350px" height="350px" color="green">
+            <v-icon>mdi-home</v-icon>
+            <v-card-title>
+              Titulo
+            </v-card-title>
+            <v-card-subtitle>
+              Subtitle
+            </v-card-subtitle>
+          </v-card>
+          <v-spacer></v-spacer>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -50,6 +142,32 @@ export default {
         this.drawer = false
       },
     },
+    barItems: [ // Currently not being used as it is hardcoded in the bar item
+      {
+        title: "Inicio",
+        value: 1
+      },
+      {
+        title: "Nosotros",
+        value: 2
+      },
+      {
+        title: "Servicios",
+        value: 3
+      },
+      {
+        title: "Equipo",
+        value: 4
+      },
+      {
+        title: "Contacto",
+        value: 5
+      },
+      {
+        title: "Acceso Clientes",
+        value: 6
+      }
+    ]
   }),
 }
 </script>
