58f3fec9c8f9955c9ab9ae613ae55adf7fbbac0f
Miguel <salixasesores@gmail.com>
2022-12-06 22:39:30 +0100

    Added final parts. Only footer is remaining. Also some code has been improved

---
+++ src/components/BarComponent.vue
@@ -0,0 +1,90 @@
+<template>
+<v-navigation-drawer
+        v-model="drawer"
+        location="right"
+        temporary
+      >
+        <v-list v-for="item in barItems" :key="item.title">
+          <v-list-item :title="item.title" :value="item.value" :prepend-icon="item.icon" v-on:click="scrollTo(item.scrollPoint)" :active="false"></v-list-item>
+        </v-list>
+        <v-list>
+          <v-list-item title="Acceso Clientes" value="6" prepend-icon="mdi-account"/>
+          <v-list-item title="Cerrar" value="7" prepend-icon="mdi-keyboard-return" @click.stop="drawer = !drawer"/>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar 
+        height="120px"
+        color="#f6fcf5"
+        hide-on-scroll
+        scroll-target="#scrolling-techniques-4"
+      >
+      <v-spacer></v-spacer>
+      <v-toolbar-side-icon>
+        <img style="height:100px" :src="require('../assets/logo.png')">
+      </v-toolbar-side-icon>
+        <v-spacer></v-spacer>
+        <v-btn class="hidden-sm-and-down" color="green" v-for="item in barItems" :key="item.title" v-on:click="scrollTo(item.scrollPoint)">{{ item.title }}</v-btn>
+        <v-btn class="hidden-sm-and-down" variant="outlined" color="teal">ACCESO CLIENTES</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon @click.stop="drawer = !drawer">
+          <v-icon class="hidden-md-and-up">mdi-menu</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <br style="height:100px;">
+      <br>
+
+</template>
+
+<script>
+export default {
+  name: 'BarComponent',
+  data: () => ({
+    drawer: false,
+    group: null,
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  barItems: [
+      {
+        value: 1,
+        title: "Inicio",
+        icon: "mdi-home",
+        scrollPoint: "inicio"
+      },
+      {
+        value: 2,
+        title: "Nosotros",
+        icon: "mdi-flower",
+        scrollPoint: "nosotros"
+      },
+      {
+        value: 3,
+        title: "Servicios",
+        icon: "mdi-star",
+        scrollPoint: "servicios"
+      },
+      {
+        value: 4,
+        title: "Equipo",
+        icon: "mdi-account-multiple",
+        scrollPoint: "equipo"
+      },
+      {
+        value: 5,
+        title: "Contacto",
+        icon: "mdi-email",
+        scrollPoint: "contacto"
+      },
+    ],
+  }),
+  methods: {
+      scrollTo(id) {
+        document.getElementById(id).scrollIntoView({ behavior: 'smooth'});
+        this.drawer = false
+      }
+    },
+}
+
+</script>
---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,76 @@
+<template>
+      <v-container grid-list-xs>
+        Llámanos o envíanos un email. Estaremos encantados de atenderte.
+        <br>
+        <br>
+        <v-list>
+          <v-list-item style="font-size: 20px;" class="text-center">
+            <v-icon color="green" >mdi-phone</v-icon> 600 600 600
+          </v-list-item>
+          <v-list-item style="font-size: 20px;" class="text-center">
+            <v-icon color="green" >mdi-email</v-icon> salixasesores@gmail.com
+          </v-list-item>
+        </v-list>
+        <br>
+        También puedes contactar directamente con alguno de nuestros departamentos. 
+        <br>
+        <br>
+        <v-row class="justify-center">
+          
+            <v-list-item v-for="department in departments" :key="department.id">
+              <h3>{{ department.name }}</h3>
+              Teléfono: {{ department.phone }}
+              <br>
+              Email: {{ department.email }}
+            </v-list-item>
+        </v-row>
+      </v-container>
+
+</template>
+
+<script>
+export default {
+  name: 'ContactComponent',
+  data: () => ({
+    departments: {
+      riesgos: {
+        id: "riesgos",
+        name: "Riesgos Laborales",
+        phone: "600 600 600",
+        email: "salixasesor@salix.com"
+      },
+      laboratorio: {
+        id: "laboratorio",
+        name: "Laboratorio",
+        phone: "600 600 600",
+        email: "salixasesor@salix.com"
+      },
+      recogida: {
+        id: "recogida",
+        name: "Recogida",
+        phone: "600 600 600",
+        email: "salixasesor@salix.com"
+      },
+      atencion: {
+        id: "atencion",
+        name: "Atención al Cliente",
+        phone: "600 600 600",
+        email: "salixasesor@salix.com"
+      },
+      administracion: {
+        id: "administracion",
+        name: "Administración",
+        phone: "600 600 600",
+        email: "salixasesor@salix.com"
+      },
+      alimentacion: {
+        id: "alimentacion",
+        name: "Alimentación",
+        phone: "600 600 600",
+        email: "salixasesor@salix.com"
+      },
+    },
+  }),
+}
+
+</script>
